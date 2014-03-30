45a254e2457df5c016e6d9a3672f79dfc70fd851
Parth <prem30488@gmail.com>
2014-03-30 15:33:02 +0530

    Create Transformation.cs

---
+++ C2CUDATranslator/Transformation.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C2CUDATranslator
+{
+    class Transformation
+    {
+
+        public String SolveIteration(ForLoop L1)
+        {
+            String Result = "";
+            return Result;
+        }
+
+    }
+}
